67143372e26b7510da4a17d08c666e4fd217571c
Paniz <paniz@Panizs-MBP.lan>
2024-02-25 20:49:56 +0100

    one-task-dag

---
+++ airflow/dags/one_task_dag.py
@@ -0,0 +1,27 @@
+""" one task dag """
+
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+default_args = {
+        'owner': 'Paniz',
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 0,
+        'catchup': False,
+        'start_date': datetime(2023, 1, 1)
+}
+
+with DAG(
+        dag_id='one_task_dag',
+        description='A one task Airflow DAG',
+        schedule_interval=None,
+        default_args=default_args
+    ) as dag:
+
+    task1 = BashOperator(
+            task_id='one_task',
+            bash_command='echo "hello, world!" > /Users/paniz/PycharmProjects/hands-on-introduction-data-engineering/lab/temp/hw_task.txt',
+            dag=dag)
